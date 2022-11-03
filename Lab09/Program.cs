b2ab0053cf32b187d652c6c57b89561f39d10a3c
Storm O'Bryant <vmarie1997@gmail.com>
2022-11-03 12:38:13 -0500

    complete at last

---
+++ Lab09/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Lab09
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      JToken[] allNeighborhoods = QuestionOne();
+      List<JToken> neighborhoodsWithNames = QuestionTwo(allNeighborhoods);
+      QuestionThree(neighborhoodsWithNames);
+      QuestionFour();
+      QuestionFive();
+    }
+
+    static void QuestionFive()
+    {
+      //Rewrite at least one of these questions only using the opposing method (example: Use LINQ Query statements instead of LINQ method calls and vice versa.)
+
+      // QuestionOne and QuestionTwo rewrite- output all neighborhoods, only neighborhoods without empty names
+      // getting here now, i'm realizing it was always just the neighborhood, not the data set for each object...i've been struggling because i can't read...
+      string file = "../data.json";
+      string jsonString = File.ReadAllText(file);
+      JObject jsonData = JObject.Parse(jsonString);
+      var query = from item in jsonData["features"].Children().ToList() 
+                  where (item["properties"]["neighborhood"].ToString() != "") 
+                  select item["properties"]["neighborhood"];
+      foreach(var item in query)
+        Console.WriteLine(item);
+
+      // rewrite QuestionThree
+      var queryTwo = query.Distinct().ToList();
+      foreach(var item in queryTwo)
+        Console.WriteLine(item);
+      Console.WriteLine(queryTwo.Count());
+    }
+
+    static void QuestionFour()
+    {
+      //Rewrite the queries from above and consolidate all into one single query.
+      // so, i need to read the text from json file and parse it into json object
+      string file = "../data.json";
+      string jsonStringData = File.ReadAllText(file);
+      JObject jsonData = JObject.Parse(jsonStringData);
+      // then create query that:
+      // outputs all data
+      // filter out neighborhoods with no names
+      // and remove the duplicates
+      // i kinda have no idea what is meant by consolidating it into a single query...
+      List<string> queriedNeighborhoods = new List<string>();
+
+      foreach (var item in jsonData["features"].Children().ToList())
+      {
+        if (item["properties"]["neighborhood"].ToString() != "")
+        {
+          queriedNeighborhoods.Add(item["properties"]["neighborhood"].ToString());
+        }
+      }
+      List<string> queriedWithNoDuplicates = queriedNeighborhoods.Distinct().ToList();
+      foreach (var item in queriedWithNoDuplicates)
+        Console.WriteLine(item);
+      Console.WriteLine($"Consolidated query: {queriedWithNoDuplicates.Count()}");
+    }
+
+    static void QuestionThree(List<JToken> neighborhoodsWithNames)
+    {
+      //Remove the duplicates (Final Total: 39 neighborhoods)
+
+      //okay, after relooking at the instructions, it says to filter out neighborhoods, which i'm now realizing is probably just the neighborhoods property of the object we have, not the entire object itself...so i will now take a different approach
+      List<string> Neighborhoods = new List<string>();
+      foreach (var item in neighborhoodsWithNames)
+      {
+        Neighborhoods.Add(item["properties"]["neighborhood"].ToString());
+      }
+      List<string> noDuplicates = Neighborhoods.Distinct().ToList();
+      foreach (var item in noDuplicates)
+        Console.WriteLine($"{item}");
+      Console.WriteLine($"No duplicates: {noDuplicates.Count()}");
+    }
+
+    static List<JToken> QuestionTwo(JToken[] allNeighborhoods)
+    {
+      //Filter out all the neighborhoods that do not have any names (Final Total: 143)
+      Console.WriteLine("=====Question Two=====\nOutput only neighborhoods with names in this data list, should be 147 neighborhoods");
+      List<JToken> noNames = new List<JToken>();
+      foreach (var item in allNeighborhoods)
+      {
+        if (item["properties"]["neighborhood"].ToString() != "")
+          noNames.Add(item);
+      }
+      foreach (var item in noNames)
+        Console.WriteLine(item.ToString());
+      Console.WriteLine($"Number of neighborhoods with names: {noNames.Count}");
+      return noNames;
+    }
+
+    static JToken[] QuestionOne()
+    {
+      //Output all of the neighborhoods in this data list (Final Total: 147 neighborhoods)
+      int counter = 0;
+      string file = "../data.json";
+      string jsonStringData = File.ReadAllText(file);
+      // Console.WriteLine(jsonStringData);
+
+      // create instance of jobject, which takes our json string and parses it into a json object
+      JObject jsonData = JObject.Parse(jsonStringData);
+      // we need all the objects in the features object of our data.json file, so target features, all it's children, and convert it to an array of jtokens so we can iterate through it later
+      JToken[] jsonDataNeighborhoods = jsonData["features"].Children().ToArray();
+      Console.WriteLine("=====Question One=====\nOutput all neighborhoods in this data list, should be 147 neighborhoods");
+      foreach (var item in jsonDataNeighborhoods)
+      {
+        Console.WriteLine(item.ToString());
+        counter++;
+      }
+      Console.WriteLine($"Number of neighborhoods: {counter}");
+      return jsonDataNeighborhoods;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,17 +1,32 @@
 # Lab09_LINQ
-Assets for lab work included
 
-# Program.cs 
-includes the code snippets we looked at and created. The earlier demo was moved to the FileIO method while the current Main executes our theater json manipulation. (Sorry for the janky formatting, I'll be updating my settings to standardize spacing and alignment in the future.)
+## What is this?
 
-# JsonClasses.cs 
-contains class definitions for the json objects we were working with.
+This repository is Lab 09 in my 401 Course. The purpose is to support and strengthen my knowledge of LINQ queries as well as provide better understanding of using json files in C# projects. Lab09 is comprised of 5 different questions to be answered, all building upon one another and relying on a json file that we must query through.
 
-# Demo.json 
-includes the data.json from the lab
+## What does it look like?
 
-# Theater.json 
-includes what we created in class
+There's a pretty standard json file comprised of different location objects(addresses, types, geometry, etc.). Most of the data is duplicates, but in all is around 149 differnt objects. I decided to answer each question as it's own method to separate and clean up my code, with each question method being called in the Main method of my Program file:
 
-# A .drawio file 
-is included that can be opened at app.diagrams.net for reference and further updates on your own
+```csharp
+static void Main(string[] args)
+    {
+      JToken[] allNeighborhoods = QuestionOne();
+      List<JToken> neighborhoodsWithNames = QuestionTwo(allNeighborhoods);
+      QuestionThree(neighborhoodsWithNames);
+      QuestionFour();
+      QuestionFive();
+    }
+```
+
+Since most of the methods/questions rely on one another, I chose to return data from differnt methods in order to use them in the next ones( as shown above).
+
+The final output is simply a list of neighborhoods from the json file, with no duplicates: so going from 147 neighborhoods down to 39.
+
+## How do you use it?
+
+The only thing you'd need to do to run this project is cd into the "Lab09" file, type "dotnet run" in your terminal and click "enter"
+
+## Any Relevant Details?
+
+Not really...Happy reminder to make sure you read instructions thoroughly. I spent 3 days struggling with the same question before realizing I had misread something simple.
